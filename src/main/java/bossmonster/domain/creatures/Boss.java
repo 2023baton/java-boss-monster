67565406bd699c797b4cbb18f750769f700e3cb3
howudong <tjdvy963@naver.com>
2023-09-23 22:37:10 +0900

    feat : Boss 클래스 추가 및 테스트 완료

---
+++ src/main/java/bossmonster/domain/creatures/Boss.java
@@ -0,0 +1,18 @@
+package bossmonster.domain.creatures;
+
+public final class Boss extends Creature {
+
+    private static final int BOSS_MIN_HP = 100;
+    private static final int BOSS_MAX_HP = 300;
+
+    public Boss(int hp) {
+        validateHp(hp);
+        this.hp = hp;
+    }
+
+    private void validateHp(int hp) {
+        if (hp < BOSS_MIN_HP || hp > BOSS_MAX_HP) {
+            throw new IllegalArgumentException("보스의 HP는 100 ~ 300 사이로 설정해야합니다.");
+        }
+    }
+}
