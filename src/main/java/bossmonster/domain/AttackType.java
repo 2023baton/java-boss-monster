a14b177dd8180e871654e53216b0ae1957250f11
howudong <tjdvy963@naver.com>
2023-09-25 11:17:09 +0900

    feat : AttackType이 데미지와 마나 소비량 등 모든 정보를 가지도록함

--- src/main/java/bossmonster/domain/AttackType.java
+++ src/main/java/bossmonster/domain/AttackType.java
@@ -3,17 +3,29 @@ package bossmonster.domain;
 import java.util.Arrays;
 
 public enum AttackType {
-    NORMAL(1),
-    MAGIC(2);
+    NORMAL(1, 10, -10),
+    MAGIC(2, 20, 30);
 
     private final int typeNumber;
+    private final int damage;
+    private final int mpCost;
 
-    AttackType(int typeNumber) {
+    AttackType(int typeNumber, int damage, int mpCost) {
         this.typeNumber = typeNumber;
+        this.damage = damage;
+        this.mpCost = mpCost;
     }
 
     public int getTypeNumber() {
         return typeNumber;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getMpCost() {
+        return mpCost;
     }
 
     public static AttackType createAttackTypeByNumber(int number) {
