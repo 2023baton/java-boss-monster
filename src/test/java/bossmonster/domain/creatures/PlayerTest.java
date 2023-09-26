95fc19142fd474452995e42749912a729b221a75
howudong <tjdvy963@naver.com>
2023-09-26 17:01:01 +0900

    refactor: 리팩토링한 코드에 따른 PlayerTest코드 리팩토링

--- src/test/java/bossmonster/domain/creatures/PlayerTest.java
+++ src/test/java/bossmonster/domain/creatures/PlayerTest.java
@@ -1,7 +1,7 @@
 package bossmonster.domain.creatures;
 
-import bossmonster.domain.AttackEntity;
-import bossmonster.domain.AttackType;
+import bossmonster.domain.attack.AttackEntity;
+import bossmonster.domain.attack.AttackType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +12,15 @@ class PlayerTest {
     @Test
     @DisplayName("hp와 mp의 합이 200을 초과하면 예외가 발생한다.")
     void 스탯합_200_초과() {
-        assertThrows(IllegalArgumentException.class, () -> new Player(100, 200, "TEST1"));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Player(100, 200, "TEST1"));
     }
 
     @Test
     @DisplayName("hp와 mp이 200합이 미만이면 예외가 발생한다.")
     void 스탯합_200_미만() {
-        assertThrows(IllegalArgumentException.class, () -> new Player(100, 99, "TEST2"));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Player(100, 99, "TEST2"));
     }
 
     @Test
@@ -37,7 +39,8 @@ class PlayerTest {
     @Test
     @DisplayName("6글자를 입력하면 예외가 발생한다.")
     void 다섯글자_이름_초과() {
-        assertThrows(IllegalArgumentException.class, () -> new Player(60, 140, "여섯글자이름"));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Player(60, 140, "여섯글자이름"));
     }
 
     @Test
@@ -56,7 +59,7 @@ class PlayerTest {
     void Mp_정상적으로_감소() {
         //given
         Player player = new Player(100, 100, "test1");
-        AttackEntity attackEntity = new AttackEntity.AttackEntityBuilder(AttackType.NORMAL).build();
+        AttackEntity attackEntity = AttackEntity.createAttack(AttackType.NORMAL);
         //when
         player.attack(attackEntity, player);
         //then
@@ -69,7 +72,7 @@ class PlayerTest {
         //given
         Player player = new Player(200, 0, "test1");
         //when
-        AttackEntity attackEntity = new AttackEntity.AttackEntityBuilder(AttackType.MAGIC).build();
+        AttackEntity attackEntity = AttackEntity.createAttack(AttackType.MAGIC);
         //then
         assertThrows(IllegalArgumentException.class, () -> player.attack(attackEntity, player));
     }
@@ -80,9 +83,9 @@ class PlayerTest {
         //given
         Player player = new Player(100, 100, "test1");
         //when
-        AttackEntity attackEntity = new AttackEntity.AttackEntityBuilder(AttackType.MAGIC).build();
+        AttackEntity attackEntity = AttackEntity.createAttack(AttackType.MAGIC);
         player.attack(attackEntity, player);
-        attackEntity = new AttackEntity.AttackEntityBuilder(AttackType.NORMAL).build();
+        attackEntity = AttackEntity.createAttack(AttackType.NORMAL);
         player.attack(attackEntity, player);
         //then
         assertThat(player.getMp()).isEqualTo(80);
